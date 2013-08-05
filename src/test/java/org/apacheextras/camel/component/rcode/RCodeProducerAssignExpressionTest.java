6d6876d7e92f4b86707eb9e43065fb53a1065921
Christoph Emmersberger <cemmersb@googlemail.com>
2013-08-05 08:30:31 +0200

    + add assign expression test

--- src/test/java/org/apacheextras/camel/component/rcode/RCodeProducerAssignExpressionTest.java
+++ src/test/java/org/apacheextras/camel/component/rcode/RCodeProducerAssignExpressionTest.java
@@ -17,40 +17,43 @@ package org.apacheextras.camel.component.rcode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.Test;
 import static org.mockito.Mockito.doAnswer;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPString;
 
 /**
  *
  * @author cemmersb
  */
-public class RCodeProducerAssignTest extends RCodeProducerTest {
+public class RCodeProducerAssignExpressionTest extends RCodeProducerTest {
 
   @Test
-  public void sendAssignVariableTest() throws Exception {
-    final Map<String, String> assignments = new HashMap<String, String>();
-    assignments.put("var1", "3 * 5");
-
+  public void sendAssignExpressionTest() throws Exception {
+    final Map<String, REXP> assignments = new HashMap<String, REXP>();
+    assignments.put("x", new REXPString("2^2"));
+    
     final MockEndpoint mockEndpoint = getMockEndpoint("mock:rcode");
     mockEndpoint.expectedBodiesReceived(null);
-
-    for (Entry<String, String> assignment : assignments.entrySet()) {
+    
+    for (Map.Entry<String, REXP> assignment : assignments.entrySet()) {
       doAnswer(new Answer<Void>() {
         @Override
         public Void answer(InvocationOnMock invocation) {
           return null;
         }
       }).when(rConnection).assign(assignment.getKey(), assignment.getValue());
-      template.sendBody("direct:rcode", assignment);
-      mockEndpoint.assertIsSatisfied();
+    template.sendBody("direct:rcode", assignment);
     }
+    
+    mockEndpoint.assertIsSatisfied();
+    
   }
-
+  
   @Override
   protected RouteBuilder createRouteBuilder() throws Exception {
     return new RouteBuilder() {
@@ -60,9 +63,9 @@ public class RCodeProducerAssignTest extends RCodeProducerTest {
         onException(Exception.class)
             .handled(true)
             .to("mock:error");
-        // Send commands to the RCode endpoint, operation is 'assign_content'
+        // Send commands to the RCode endpoint, operation is 'assign_expression'
         from("direct:rcode")
-            .to("rcode:localhost:6311/assign_content?user=test&password=test123&bufferSize=10")
+            .to("rcode:localhost:6311/assign_expression?user=test&password=test123&bufferSize=10")
             .to("mock:rcode");
       }
     };
